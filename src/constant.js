e8cc9f7a41a47574f79939e8e18b4c5df7869d58
ALI JAN <133298273+AliJanGithub@users.noreply.github.com>
2025-01-28 01:12:09 +0500

    first commit

--- src/constant.js
+++ src/constant.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
 export const URL='https://attendance-lime-nine.vercel.app'
-=======
-export const URL='https://ase-1b1zy1jc7-ali-jameels-projects.vercel.app'
->>>>>>> 4158db164210fa538bcabff8b2599b0129e32635
